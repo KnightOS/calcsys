391b9a9880685561ef37e6b1a0988f02f4dce062
Drew DeVault <sir@cmpwn.com>
2015-01-21 16:07:23 -0700

    Implemented a selected value in hex editor

--- ui/hexedit.asm
+++ ui/hexedit.asm
@@ -30,19 +30,69 @@ _:
     jr z, .down
     cp kUp
     jr z, .up
+    cp kLeft
+    kjp(z, .left)
+    cp kRight
+    kjp(z, .right)
     jr .loop
 .down:
+    kld(hl, hexedit_offset)
+    ld a, (hl)
+    add a, 8
+    ld (hl), a
+    bit 6, a ; Check for overflow
+    jr z, .redraw
+    ld a, (hl)
+    add a, -8
+    ld (hl), a
     kld(hl, (hexedit_address))
     ld bc, 8
     add hl, bc
     kld((hexedit_address), hl)
     jr .redraw
 .up:
+    kld(hl, hexedit_offset)
+    ld a, (hl)
+    add a, -8
+    ld (hl), a
+    bit 6, a ; Check for overflow
+    jr z, .redraw
+    ld a, (hl)
+    add a, 8
+    ld (hl), a
     kld(hl, (hexedit_address))
     ld bc, -8
     add hl, bc
     kld((hexedit_address), hl)
     jr .redraw
+.left:
+    kld(hl, hexedit_offset)
+    ld a, (hl)
+    dec a
+    ld (hl), a
+    bit 6, a ; Check for overflow
+    kjp(z, .redraw)
+    ld a, 7
+    ld (hl), a
+    kld(hl, (hexedit_address))
+    ld bc, -8
+    add hl, bc
+    kld((hexedit_address), hl)
+    kjp(.redraw)
+.right:
+    kld(hl, hexedit_offset)
+    ld a, (hl)
+    inc a
+    ld (hl), a
+    bit 6, a ; Check for overflow
+    kjp(z, .redraw)
+    ld a, 64 - 8
+    ld (hl), a
+    kld(hl, (hexedit_address))
+    ld bc, -8
+    add hl, bc
+    kld((hexedit_address), hl)
+    kjp(.redraw)
 
 draw_hex_page:
     ld de, 0x0208
@@ -66,12 +116,30 @@ draw_hex_page:
     pcall(newline)
     ld d, 2
     djnz .outer_loop
+    ; Highlight selected value
+    kld(a, (hexedit_offset))
+    ld c, a
+    rra \ rra \ rra ; A /= 8
+    and 0b00011111
+    add a, a \ ld b, a \ add a, a \ add a, b ; A *= 6
+    add a, 8
+    ld l, a ; Y
+    ld a, c
+    and 0b111
+    ld c, a
+    add a, a \ add a, a \ add a, a \ add a, c
+    add a, 23
+    ld e, a ; X
+    ld bc, (5 << 8) + 9
+    pcall(rectXOR)
     ret
 
 hexedit_address:
     .dw 0
 hexedit_offset:
-    .dw 0
+    .db 0
+hexedit_display_mode:
+    .db 0
 
 hexedit_toggle_flash:
     in a, (2) ; Check flash
@@ -85,22 +153,30 @@ hexedit_toggle_flash:
     pcall(unlockFlash)
     kjp(hex_editor)
 
+hexedit_switch_display:
+    kld(hl, (hexedit_display_mode))
+    inc hl
+    kjp(hex_editor)
+
 hexedit_corelib_menu_flash_locked:
     .db 55 ; Width of menu
-    .db 4
+    .db 5
     .db "Go to address", 0
     .db "Memory banks", 0
     .db "Unlock Flash", 0
+    .db "Switch display", 0
     .db "Back", 0
 hexedit_corelib_menu_flash_unlocked:
     .db 55 ; Width of menu
-    .db 4
+    .db 5
     .db "Go to address", 0
     .db "Memory banks", 0
     .db "Lock Flash", 0
+    .db "Switch display", 0
     .db "Back", 0
 hexedit_corelib_menu_actions:
     .dw menu_main ;.dw hexedit_goto
     .dw menu_main ;.dw hexedit_banks
     .dw hexedit_toggle_flash
+    .dw hexedit_switch_display
     .dw menu_main
